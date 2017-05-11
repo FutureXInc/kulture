bc2e32c9e574b7869eb12ab95abf64252f5dcae1
Pattanashetty, Sadananda <Sadananda_Pattanashetty@intuit.com>
2017-05-10 23:00:33 -0700

    First

--- Kultur/Kulture/ViewController.swift
+++ Kultur/Kulture/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let api = API()
+        api.userLogin(password: "biswa", userName: "biswa", successFunc: { (user: PFUser) in
+//            api.savePost(postType: .Text, caption: "text post", kidUserId: "kid1", text: "Hi")
+//            api.savePost(postType: .Image, caption: "image post", kidUserId: "kid2",
+//                         image: UIImage(named: "kultureTree"))
+//            api.savePost(postType: .Video, caption: "video post", kidUserId: "kid3",
+//                         videoId: "KnoWRBtJ2Fo")
+
+            api.fetchApprovedPostsForKid(kidUserId: "RM2F0AcPSo",
+                                         successFunc: { (posts: [PFObject]?) in
+                                            print()
+                            }, errorFunc: { (error) in
+                                print("\(error)")
+                            })
+
+
+        })
     }
 
     override func didReceiveMemoryWarning() {
