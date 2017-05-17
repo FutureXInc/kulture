b826f7b687626ca603c9be1874c322866001366b
bis <biswa.panda@gmail.com>
2017-05-16 19:58:25 -0700

    add user cache

---
+++ Kultur/Kulture/UserCache.swift
@@ -0,0 +1,16 @@
+//
+//  UserCache.swift
+//  Kulture
+//
+//  Created by bis on 5/16/17.
+//  Copyright © 2017 FutureXInc. All rights reserved.
+//
+
+import UIKit
+
+class UserCache: NSObject {
+    
+    func getUser(_ userId: String) -> User? {
+        return nil
+    }
+}
